b5f172940cb9a9fc0bad88af918aba798df2236e
Pushpendra paliwal <ppaliwal304@gmail.com>
2024-10-04 19:11:07 +0530

    Framework initial code

---
+++ src/main/java/org/example/endpoints/APIConstants.java
@@ -0,0 +1,10 @@
+package org.example.endpoints;
+
+public class APIConstants {
+    // Endpoints -- keep all endpoints urls-Post,Put,Delete,Patch,Head
+
+    public static String BASE_URL  = "https://restful-booker.herokuapp.com";
+    public static String CREATE_UPDATE_BOOKING_URL  = "/booking";
+    public static String AUTH_URL  = "/auth";
+    public static String PING_URL  = "/ping";
+}
